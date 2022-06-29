8a353f77da6fd00375b1ff6bcd5b9d19656fe23c
Diego Binagi <diegobinagi@gmail.com>
2022-06-29 08:54:56 -0300

    Fix Mac installer

--- install_mac.sh
+++ install_mac.sh
@@ -1,7 +1,7 @@
 cp init.vim ~/.config/nvim
 cp ginit.vim ~/.config/nvim
 
-cp -R autoload ~/.config/nvim/autoload
-cp -R colors ~/.config/nvim/colors
-cp -R lua ~/.config/nvim/lua
-cp -R syntax ~/.config/nvim/syntax
+cp -R autoload ~/.config/nvim
+cp -R colors ~/.config/nvim
+cp -R lua ~/.config/nvim
+cp -R syntax ~/.config/nvim
