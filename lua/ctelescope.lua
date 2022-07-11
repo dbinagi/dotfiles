3e3004e57846f6680d55c91a475dac3ec1a08b17
Diego Binagi <diegobinagi@gmail.com>
2022-07-11 12:26:47 -0300

    Change theme for telescope

--- lua/ctelescope.lua
+++ lua/ctelescope.lua
@@ -1,9 +1,19 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
+require('telescope').setup({
+  defaults = {
+    layout_config = {
+      vertical = { width = 0.9 }
+    },
+  },
+})
+
+map('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files({layout_strategy='vertical'})<cr>", opts)
 map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
 map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', opts)
 map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', opts)
 
-map('n', '<leader>fgs', "<cmd>lua require('telescope.builtin').git_status()<cr>", opts)
+map('n', '<leader>fs', "<cmd>lua require('telescope.builtin').git_status()<cr>", opts)
+map('n', '<leader>fl', "<cmd>lua require('telescope.builtin').lsp_definitions()<cr>", opts)
+
