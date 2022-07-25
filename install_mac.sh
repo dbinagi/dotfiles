b5f9e0464d4e685ec62f0a7318de953a0b61b8ae
Diego Binagi <diegobinagi@gmail.com>
2022-07-25 18:02:02 -0300

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -7,6 +7,7 @@ cp -R nvim/lua ~/.config/nvim
 cp -R nvim/syntax ~/.config/nvim
 
 cp -R fish ~/.config
+cp -R omf ~/.config
 cp -R tmux/.tmux.conf ~/
 
 mkdir -p ~/.config/alacritty && cp alacritty/alacritty_mac.yml ~/.config/alacritty/alacritty.yml
