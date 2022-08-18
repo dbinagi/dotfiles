0cc69452dda4247fe9ebec3fa0fd2789bafc7d69
Diego Binagi <diegobinagi@gmail.com>
2022-08-17 23:04:02 -0300

    Update install_wsl.bat

--- install_wsl.bat
+++ install_wsl.bat
@@ -6,7 +6,16 @@ cd ..
 xcopy nvim_tmp \\wsl$\Ubuntu\home\bini\.config\nvim /E/H/C/I/Y
 
 xcopy fish \\wsl$\Ubuntu\home\bini\.config\fish /E/H/C/I/Y
-xcopy omf \\wsl$\Ubuntu\home\bini\.config\omf /E/H/C/I/Y
+
+# xcopy omf \\wsl$\Ubuntu\home\bini\.config\omf /E/H/C/I/Y
+
+xcopy omf omf_tmp /E/H/C/I/Y
+cd omf_tmp
+for /R %%G in (*) do dos2unix -n "%%G" "%%G"
+cd ..
+
+xcopy omf_tmp \\wsl$\Ubuntu\home\bini\.config\omf /E/H/C/I/Y
+
 xcopy dircolors\.dircolors \\wsl$\Ubuntu\home\bini\.dircolors* /Y
 
 dos2unix -n tmux\.tmux_wsl.conf tmux\.tmux_tmp.conf
@@ -16,5 +25,6 @@ DEL /F /Q tmux\.tmux_tmp.conf
 xcopy alacritty\alacritty_windows.yml %APPDATA%\alacritty\alacritty.yml* /Y
 
 rmdir /s /q nvim_tmp
+rmdir /s /q omf_tmp
 
 set /p DUMMY=Hit ENTER to continue...
