c736da905fce9d73147830e8c7d33dc7533e1502
Bini <>
2023-04-22 22:16:04 -0300

    Script for installation and omf plugins for nodejs

--- omf/init.fish
+++ omf/init.fish
@@ -1,2 +1,5 @@
 
 set -gx PROJECT_PATHS /mnt/d/dev /mnt/disk2/dev/repo /mnt/disk1/dev/repo
+
+# Add path for nvm
+set -gx NVM_DIR ~/nvm
---
+++ script.sh
@@ -0,0 +1,6 @@
+sudo apt-get install omf
+sudo apt-get install ripgrep
+sudo apt install build-essential
+sudo apt-get install unzip
+omf install
+omf update
