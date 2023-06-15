3d310bca93d3d2832c74a44f2d8ee0ddd4906b79
Bini <>
2023-06-14 23:12:34 -0300

    Check directory paths for omf - pj

--- omf/init.fish
+++ omf/init.fish
@@ -1,5 +1,16 @@
+set -gx PROJECT_PATHS
 
-set -gx PROJECT_PATHS /mnt/d/dev /mnt/disk2/dev/repo /mnt/disk1/dev/repo
+if test -d /mnt/d/dev
+    set -gx PROJECT_PATHS $PROJECT_PATHS /mnt/d/dev
+end
+
+if test -d /mnt/disk2/dev/repo
+    set -gx PROJECT_PATHS $PROJECT_PATHS /mnt/disk2/dev/repo
+end
+
+if test -d /mnt/disk1/dev/repo
+    set -gx PROJECT_PATHS $PROJECT_PATHS /mnt/disk1/dev/repo
+end
 
 # Add path for nvm
 set -gx NVM_DIR ~/nvm
